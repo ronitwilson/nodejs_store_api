12380e47265bce29ef21c13c90642f034c967fda
Ronit Wilson <ronit.study@gmail.com>
2023-09-28 13:10:46 +0530

    add a filter with query string

--- controller/products.js
+++ controller/products.js
@@ -1,13 +1,13 @@
-const products = require("../model/products")
 const Product = require("../model/products")
 
 const getAllProductsStatic = async (req, res) => {
-    res.status(200).json({msg: 'get all products static route'})
+    getProduct =await Product.find({featured: true, name: "a first wooden table"})
+    res.status(200).json({msg: 'get all products static route', getProduct, nbHits: getProduct.length})
 }
 
 const getAllProducts = async (req, res) => {
-    getProduct =await Product.find({featured: true, name: "a first wooden table"})
-    res.status(200).json({getProduct, nbHits: getProduct.length})
+    products =await Product.find(req.query)
+    res.status(200).json({products, nbHits: products.length})
 }
 
 module.exports = {
