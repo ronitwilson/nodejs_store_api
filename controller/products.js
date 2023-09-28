36211a9b7d6e52ba6a6dfaba69874bbb4c0fe472
Ronit Wilson <ronit.study@gmail.com>
2023-09-28 15:40:48 +0530

    use regex for querying

--- controller/products.js
+++ controller/products.js
@@ -6,11 +6,17 @@ const getAllProductsStatic = async (req, res) => {
 }
 
 const getAllProducts = async (req, res) => {
-    const {featured} = req.query
+    const {featured, company, name} = req.query
     const queryObj = {}
     if (featured === 'true') {
         console.log(featured)
        queryObj.featured = featured==='true' ? true: false
+    }
+    if (company) {
+        queryObj.company = company
+    }
+    if (name) {
+        queryObj.name = { $regex: name, $options: 'i'}
     }
     console.log(queryObj)
     products =await Product.find(queryObj)
