cabf10fd2c05e22023da06f88a0434b7edf010c8
Ronit Wilson <ronit.study@gmail.com>
2023-09-29 11:35:41 +0530

    use numeric sorting

--- controller/products.js
+++ controller/products.js
@@ -1,12 +1,12 @@
 const Product = require("../model/products")
 
 const getAllProductsStatic = async (req, res) => {
-    getProduct =await Product.find({featured: true, name: "a first wooden table"})
+    getProduct =await Product.find({featured: true, name: "a first wooden table", price:{$gt: 30} })
     res.status(200).json({msg: 'get all products static route', getProduct, nbHits: getProduct.length})
 }
 
 const getAllProducts = async (req, res) => {
-    const {featured, company, name, sort} = req.query
+    const {featured, company, name, sort, numericFilters} = req.query
     const queryObj = {}
     if (featured === 'true') {
         console.log(featured)
@@ -18,6 +18,29 @@ const getAllProducts = async (req, res) => {
     if (name) {
         queryObj.name = { $regex: name, $options: 'i'}
     }
+    if (numericFilters) {
+        const operatorMap = {
+            '>': '$gt',
+            '>=': '$gte',
+            '=': '$eq',
+            '<': '$lt',
+            '<=': '$lte',
+        }
+
+    const Regex = /\b(<|>|>=|=|<|<=)\b/g
+    let filters = numericFilters.replace(Regex, (match) => {
+        return `-${operatorMap[match]}-`
+    })
+    console.log(filters)
+    const options = ['price', 'rating']
+    filters = filters.split(",").forEach((item) => {
+        const [field, operator, number] = item.split("-")
+        if(options.includes(field)){
+            queryObj[field] = {[operator]: Number(number)}
+        }
+    })
+    }
+
     console.log(queryObj)
     let result =  Product.find(queryObj)
     if (sort) {
@@ -28,6 +51,11 @@ const getAllProducts = async (req, res) => {
     } else {
         result.sort("createdAt")
     }
+
+    const page = Number(req.query.page) || 1
+    const limit = Number(req.query.limit) || 10
+    const skip = (page - 1) * limit
+    result  = result.limit(limit).skip(skip)
     const products = await result
     res.status(200).json({products, nbHits: products.length})
 }
